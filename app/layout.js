ea52a88426c1c05e68a2fd3450dcb6210c78d393
Soukarya <37843094+Soukarya@users.noreply.github.com>
2026-09-08 20:11:02 +0530

    Load language dropdown guard last

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ import './saffron-monogram.css';
 import './invitation-controls.css';
 import './viewport-validation-fixes.css';
 import './blush-compact-polish.css';
+import './language-dropdown.css';
 
 const bengali = localFont({ src: './fonts/noto-serif-bengali.ttf', variable: '--font-bengali', display: 'swap', preload: false });
 const devanagari = localFont({ src: './fonts/noto-serif-devanagari.ttf', variable: '--font-devanagari', display: 'swap', preload: false });
