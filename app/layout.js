5c2fe603aa81b3f2568216bab9d86e62edafef29
Soukarya <37843094+Soukarya@users.noreply.github.com>
2026-09-09 21:02:52 +0530

    fix: load Samsung Internet compatibility layer last

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ import './inside-right-saffron-parity.css';
 import './inside-left-saffron-parity.css';
 import './saffron-front-separator.css';
 import './front-saffron-parity.css';
+import './samsung-internet-compat.css';
 
 const bengali = localFont({ src: './fonts/noto-serif-bengali.ttf', variable: '--font-bengali', display: 'swap', preload: false });
 const devanagari = localFont({ src: './fonts/noto-serif-devanagari.ttf', variable: '--font-devanagari', display: 'swap', preload: false });
